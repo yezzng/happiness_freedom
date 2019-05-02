f9cea37bec8467ea47ea7958455b6ac0c2446a64
Kathy Wang <kw496@cornell.edu>
2019-05-02 16:44:21 -0400

    debugging for map

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ var projection = d3.geoMercator()
   .scale(125)
   .translate([width / 2.5, height / 1.4]);
 
-var svg = d3.select( "#map" ).append( "svg" )
+var svg = d3.select("#map").append("svg")
   .attr("width", width)
-  .attr("height", height)
-  .attr("class", "mapSVG");
+  .attr("height", height);
+  // .attr("class", "mapSVG");
 
 var path = d3.geoPath()
   .projection(projection);
-
 }
