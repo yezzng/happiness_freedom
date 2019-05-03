6534e97c27caa3f7b3ac202ccddfe00886a20ed8
VArunie <bajpai.varunie@gmail.com>
2019-05-03 09:01:29 -0400

    upload

--- script1.js
+++ script1.js
@@ -4,15 +4,7 @@
   d3.csv("../data/2015happyFreedom.csv").then( function(data) {
 
     
-    // clean up data
-    var filtered = data.filter(d => d['HumanFreedomRank'] !== NaN &&
-    d['HumanFreedomRank'] > 0 &&
-    d['HumanFreedomRank'].length !== 0 &&
-    d['HumanFreedomScore'] !== NaN && 
-    d['HumanFreedomScore'] > 0 && 
-    d['HumanFreedomScore'].length !== 0);
-
-    filtered.forEach( (d, i) => {
+    data.forEach( (d, i) => {
       d['HumanFreedomScore'] =  Number(d["HumanFreedomScore"])  ;
       d['HumanFreedomRank'] =  Number(d["HumanFreedomRank"])   ;
       d['HappinessRank'] =  Number(d["HappinessRank"])   ;
@@ -23,47 +15,44 @@
 
     console.log(data);
 
-
-
-
-    let svg2 = d3.select("#scatterplot");
-    let width2 = svg2.attr("width");
-    let height2 = svg2.attr("height");
-    let margin2 = { top: 10, right: 10, bottom: 50, left:50};
-    let chartWidth2 = width2 - margin2.left - margin2.right; 
-    let chartHeight2 = height2 - margin2.top - margin2.bottom;
+    let svg = d3.select("svg#scatterplot");
+    let width = svg.attr("width");
+    let height = svg.attr("height");
+    let margin = { top: 10, right: 10, bottom: 50, left:50};
+    let chartWidth = width - margin.left - margin.right; 
+    let chartHeight = height - margin.top - margin.bottom;
    
 
     
 
 
-    var g= svg2.append("g").attr("transform","translate("+ margin2.left + ","+ margin2.top + ")");
+    var g= svg.append("g").attr("transform","translate("+ margin.left + ","+ margin.top + ")");
 
-    const happyMin = d3.min(filtered, d => d['HappinessScore']);
-    const happyMax = d3.max(filtered, d => d['HappinessScore']);
+    const happyMin = d3.min(data, d => d['HappinessScore']);
+    const happyMax = d3.max(data, d => d['HappinessScore']);
     const happyScale = d3.scaleLinear()
             .domain([1, 10])
-            .range([-10, width2]); 
+            .range([0, width]); 
    
-    const freeMin = d3.min(filtered, d => d['HumanFreedomScore']);
-    const freeMax = d3.max(filtered, d => d['HumanFreedomScore']);
+    const freeMin = d3.min(data, d => d['HumanFreedomScore']);
+    const freeMax = d3.max(data, d => d['HumanFreedomScore']);
     const freeScale = d3.scaleLinear()
                     .domain([1, 10])
-                    .range([height2, 50]);
+                    .range([height,0]);
     const regionScale = d3.scaleOrdinal(d3.schemeCategory10);
 
     
 
     let leftAxis = d3.axisLeft(freeScale); // ticks looked fine here
-    svg2.append("g").attr("class", "y axis") // Not d3-required. Just helpful for styling
-      .attr("transform","translate("+ (margin2.left-10) +","+ (margin2.top - 50) +")")
+    svg.append("g").attr("class", "y axis") // Not d3-required. Just helpful for styling
+      .attr("transform","translate("+ (margin.left-10) +","+ margin.top +")")
       .call(leftAxis);
 
 
       let bottomAxis = d3.axisBottom(happyScale).ticks(10, d3.format("1")); 
   
-    let element = svg2.append("g").attr("class", "x axis")
-      .attr("transform","translate("+ (margin2.left +10) +","+ (margin2.top + chartHeight2 + 10) +")");
+    let element = svg.append("g").attr("class", "x axis")
+      .attr("transform","translate("+ margin.left +","+ (margin.top + chartHeight + 10) +")");
     bottomAxis(element); 
 
 
@@ -71,17 +60,17 @@
     .attr("class", "tooltip1")				
     .style("opacity", 1);
 
-    let scatter = svg2.append("g") // We make a subgroup to contain the points we are adding, and use the margins to shrink it and move it to the right place, so that it doesn't overlap our axes
-          .attr("transform","translate("+margin2.left+","+margin2.top+")");
+    let scatter = svg.append("g") // We make a subgroup to contain the points we are adding, and use the margins to shrink it and move it to the right place, so that it doesn't overlap our axes
+          .attr("transform","translate("+margin.left+","+margin.top+")");
 
-          filtered.forEach( (d, i) => {
+          data.forEach( (d, i) => {
 
             let happy = happyScale(d['HappinessScore']);
             let free = freeScale(d['HumanFreedomScore']);
             let region = regionScale(d['Region']);
             let country=d['Country'];
-            let happyrank=d['HappinessRank'];
-            let freerank=d['HumanFreedomRank'];
+            let happyScore=d['HappinessScore'];
+            let freeScore=d['HumanFreedomScore'];
             // Draw
             let circle = scatter.append("circle")
                   .attr("cx", happy)
@@ -96,7 +85,7 @@
                       .duration(200)		
                       .style("opacity", .9);		
                       
-                      div1.html("Country: "+country+ "<br/>"+"Happiness Score: "+happy+ "<br/>"+"Human Freedom Score: "+free)	
+                      div1.html("Country: "+country+ "<br/>"+"Happiness Score: "+happyScore+ "<br/>"+"Human Freedom Score: "+freeScore)	
                       .style("left", (d3.event.pageX) + "px")		
                       .style("top", (d3.event.pageY - 28) + "px");})
                 .on("mouseout", function(d) {		
