4bfe2546f26b2bb3e8c3d1a97c3bf0a4650198b0
Kathy Wang <kw496@cornell.edu>
2019-05-02 22:03:09 -0400

    deleted unnecessary

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let svg = d3.select("#map");
 let width = svg.attr("width");
 let height = svg.attr("height");
@@ -34,28 +35,3 @@ console.log(world);
         .attr("d", path);
 };
 requestData();
-
-
-
-// window.onload = () => {
-//   width =  962, height = 600
-//
-//
-// var projection = d3.geoMercator()
-//   .scale(125)
-//   .translate([width / 2.5, height / 1.4]);
-//
-// var svg = d3.select("#map").append("svg")
-//   .attr("width", width)
-//   .attr("height", height);
-//   // .attr("class", "mapSVG");
-//
-// var path = d3.geoPath();
-//
-// var projection = d3.geoMercator()
-//                    .scale(130)
-//                   .translate( [width / 2, height / 1.5]);
-//
-// var path = d3.geoPath().projection(projection);
-//
-// }
