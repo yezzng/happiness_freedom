de3da023fd78a6b02beb41eb5c74fa7df4225f7c
Kathy Wang <kw496@cornell.edu>
2019-05-12 21:24:13 -0400

    code fixes

--- scatter.js
+++ scatter.js
@@ -6,10 +6,10 @@
     var filtered = data.filter(d => d['HumanFreedomRank'] !== NaN &&
     d['HumanFreedomRank'] > 0 &&
     d['HumanFreedomRank'].length !== 0 &&
-    d['HumanFreedomScore'] !== NaN && 
-    d['HumanFreedomScore'] > 0 && 
+    d['HumanFreedomScore'] !== NaN &&
+    d['HumanFreedomScore'] > 0 &&
     d['HumanFreedomScore'].length !== 0);
-    
+
     filtered.forEach( (d, i) => {
       d['HumanFreedomScore'] =  Number(d["HumanFreedomScore"])  ;
       d['HumanFreedomRank'] =  Number(d["HumanFreedomRank"])   ;
@@ -25,11 +25,11 @@
     let width = svg.attr("width");
     let height = svg.attr("height");
     let margin = { top: 10, right: 10, bottom: 50, left:50};
-    let chartWidth = width - margin.left - margin.right; 
+    let chartWidth = width - margin.left - margin.right;
     let chartHeight = height - margin.top - margin.bottom;
-   
 
-    
+
+
 
     var g= svg.append("g").attr("transform","translate("+ margin.left + ","+ margin.top + ")");
 
@@ -37,8 +37,8 @@
     const happyMax = d3.max(filtered, d => d['HappinessScore']);
     const happyScale = d3.scaleLinear()
             .domain([1, 10])
-            .range([0, chartWidth]); 
-   
+            .range([0, chartWidth]);
+
     const freeMin = d3.min(filtered, d => d['HumanFreedomScore']);
     const freeMax = d3.max(filtered, d => d['HumanFreedomScore']);
     const freeScale = d3.scaleLinear()
@@ -46,7 +46,7 @@
                     .range([chartHeight,0]);
     const regionScale = d3.scaleOrdinal(d3.schemeCategory10);
 
-    
+
 
     let leftAxis = d3.axisLeft(freeScale); // ticks looked fine here
     svg.append("g").attr("class", "y axis") // Not d3-required. Just helpful for styling
@@ -54,11 +54,11 @@
       .call(leftAxis);
 
 
-    let bottomAxis = d3.axisBottom(happyScale).ticks(10); 
-  
+    let bottomAxis = d3.axisBottom(happyScale).ticks(10);
+
     let element = svg.append("g").attr("class", "x axis")
       .attr("transform","translate("+ (margin.left) +","+ (margin.top + chartHeight + 20) +")");
-    bottomAxis(element); 
+    bottomAxis(element);
 
 
     // labels
@@ -82,8 +82,8 @@
         .text("Happiness Score");
 
     // tooltip
-    var div1 = d3.select("body").append("div")	
-    .attr("class", "tooltip1")				
+    var div1 = d3.select("body").append("div")
+    .attr("class", "tooltip1")
     .style("opacity", 1);
 
     let scatter = svg.append("g") // We make a subgroup to contain the points we are adding, and use the margins to shrink it and move it to the right place, so that it doesn't overlap our axes
@@ -94,9 +94,9 @@
             let happy = happyScale(d['HappinessScore']);
             let free = freeScale(d['HumanFreedomScore']);
             let region = regionScale(d['Region']);
-            let country=d['Country'];
-            let happyScore=d['HappinessScore'];
-            let freeScore=d['HumanFreedomScore'];
+            let country = d['Country'];
+            let happyScore = d['HappinessScore'];
+            let freeScore = d['HumanFreedomScore'];
 
             // Draw
             let circle = scatter.append("circle")
@@ -107,19 +107,19 @@
                   .style("fill", region)
                   .attr("region", d["Region"])
                   .attr("nation", d["Country"])
-                  .on("mouseover", function(d) {		
-                    div1.transition()		
-                      .duration(200)		
-                      .style("opacity", .9);		
-                      
-                      div1.html("Country: "+country+ "<br/>"+"Happiness Score: "+happyScore+ "<br/>"+"Human Freedom Score: "+freeScore)	
-                      .style("left", (d3.event.pageX) + "px")		
+                  .on("mouseover", function(d) {
+                    div1.transition()
+                      .duration(200)
+                      .style("opacity", .9);
+
+                      div1.html("Country: "+country+ "<br/>"+"Happiness Score: "+happyScore+ "<br/>"+"Human Freedom Score: "+freeScore)
+                      .style("left", (d3.event.pageX) + "px")
                       .style("top", (d3.event.pageY - 28) + "px");})
-                .on("mouseout", function(d) {		
-                    div1.transition()		
-                      .duration(500)		
-                      .style("opacity", 0);});	
-      
+                .on("mouseout", function(d) {
+                    div1.transition()
+                      .duration(500)
+                      .style("opacity", 0);});
+
           });
 
           // create check boxes
@@ -135,14 +135,14 @@
           let box10 = d3.select("checkbox#box10");
 
 
-        
+
           // Regions
           regionScale.domain().forEach(function(d,i) {
             d3.select("#simpleLegend")
               .append("span").text(d)
               .style("color", regionScale(d))
               .on("mouseover", function() {
-                scatter.selectAll("circle").each(function() {  
+                scatter.selectAll("circle").each(function() {
                   let circle = d3.select(this);
                   if (circle.attr("region") === d) {
                     circle.attr("opacity", 0.9);
@@ -153,8 +153,8 @@
                 })
               })
           });
-          
 
-          
+
+
 
         })
